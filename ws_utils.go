a4e3e1ec786d84fad7d1330d0378feef47f3bf55
suprdamogu <suprdamogu@hotmail.com>
2020-12-24 16:22:25 +0800

    ws_utils

---
+++ ws_utils.go
@@ -0,0 +1,11 @@
+package suprlib
+
+import (
+	"golang.org/x/net/websocket"
+)
+
+func WSRead(ws *websocket.Conn) (data []byte, err error) {
+	var d []byte
+	err = websocket.Message.Receive(ws, &d)
+	return d, err
+}
