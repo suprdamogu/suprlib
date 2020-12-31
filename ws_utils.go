478116c990236110cad9a4ce071d663f08d2d662
suprdamogu <suprdamogu@hotmail.com>
2020-12-31 17:27:28 +0800

    gogogo

--- ws_utils.go
+++ ws_utils.go
@@ -4,8 +4,16 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-func WebSocketRead(ws *websocket.Conn) (data []byte, err error) {
+func NewWebsocket(url string, origin string) (*websocket.Conn, error) {
+	return websocket.Dial(url, "", origin)
+}
+
+func WebsocketRead(conn *websocket.Conn) (data []byte, err error) {
 	var d []byte
-	err = websocket.Message.Receive(ws, &d)
+	err = websocket.Message.Receive(conn, &d)
 	return d, err
+}
+
+func WebsocketWrite(conn *websocket.Conn, data []byte) error {
+	return websocket.Message.Send(conn, data)
 }
