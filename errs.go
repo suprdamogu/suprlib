9dfb52fb143fa2ef63ddc8743f75400f37304d62
suprdamogu <suprdamogu@hotmail.com>
2021-01-03 00:29:40 +0800

    not found error

--- errs.go
+++ errs.go
@@ -4,3 +4,4 @@ import "errors"
 
 var MAP_KEY_NOT_FOUND_ERROR = errors.New("MAP_KEY_NOT_FOUND_ERROR")
 var JSON_MAP_TRANS_ERROR = errors.New("JSON_MAP_TRANS_ERROR")
+var NOT_FOUND_ERROR = errors.New("NOT_FOUND_ERROR")
