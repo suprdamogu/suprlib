1003adec23203b00956a1bf1b90b1782bc1b6738
suprdamogu <suprdamogu@hotmail.com>
2021-01-19 10:44:47 +0800

    gogoogogogooo

---
+++ algo_sort.go
@@ -0,0 +1,9 @@
+package suprlib
+
+//Int64原地排序
+//sort.Sort(Int64Slice(datas))
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
