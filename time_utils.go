2ae0d60b176c497baac8573d913cb37858becaf1
suprdamogu <suprdamogu@hotmail.com>
2021-01-21 13:39:41 +0800

    gogogo

--- time_utils.go
+++ time_utils.go
@@ -5,3 +5,8 @@ import "time"
 func TimeStr(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
+
+func TimeStamp(t time.Time) int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
