b24d73f833d0057e578e2459795ecab462b1b589
suprdamogu <suprdamogu@hotmail.com>
2021-01-27 14:45:23 +0800

    gogoog

--- num_convert.go
+++ num_convert.go
@@ -9,3 +9,19 @@ func I64Str(num int64) string {
 func F64Str(num float64) string {
 	return strconv.FormatFloat(num, 'f', -1, 64)
 }
+
+func StrF64(s string) float64 {
+	f64, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic("Str to F64 Error,s:" + s)
+	}
+	return f64
+}
+
+func StrI64(s string) int64 {
+	I64, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic("Str to I64 Error,s:" + s)
+	}
+	return I64
+}
--- time_utils.go
+++ time_utils.go
@@ -10,3 +10,10 @@ func TimeStamp(t time.Time) int64 {
 	return t.UnixNano() / int64(time.Millisecond)
 }
 
+func StrTime(s string) time.Time {
+	dt, err := time.Parse("2006-01-02 15:04:05", s)
+	if err != nil {
+		panic("Str to Time Error,s:" + s)
+	}
+	return dt
+}
