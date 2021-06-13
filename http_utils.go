fec3803689244695973146f4d93181ef22c5cc43
suprdamogu <suprdamogu@hotmail.com>
2021-06-13 21:30:38 +0800

    gogoo

--- http_utils.go
+++ http_utils.go
@@ -79,3 +79,23 @@ func HttpDelete(url string, headers map[string]string, body io.Reader) ([]byte, 
 	defer resp.Body.Close()
 	return ioutil.ReadAll(resp.Body)
 }
+
+func HttpPut(url string, headers map[string]string, body io.Reader) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		return nil, err
+	}
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+	}
+	req.Close = true
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
