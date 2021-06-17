bb20c428be5ee5fc275f1eae90214c909f15d2e7
suprdamogu <suprdamogu@hotmail.com>
2021-06-17 20:39:23 +0800

    ws modify

--- ws_utils.go
+++ ws_utils.go
@@ -8,12 +8,22 @@ func NewWebsocket(url string, origin string) (*websocket.Conn, error) {
 	return websocket.Dial(url, "", origin)
 }
 
-func WebsocketRead(conn *websocket.Conn) (data []byte, err error) {
+func WebsocketReadBytes(conn *websocket.Conn) (data []byte, err error) {
 	var d []byte
 	err = websocket.Message.Receive(conn, &d)
 	return d, err
 }
 
-func WebsocketWrite(conn *websocket.Conn, data []byte) error {
+func WebsocketWriteBytes(conn *websocket.Conn, data []byte) error {
+	return websocket.Message.Send(conn, data)
+}
+
+func WebsocketReadString(conn *websocket.Conn) (data string, err error) {
+	var d string
+	err = websocket.Message.Receive(conn, &d)
+	return d, err
+}
+
+func WebsocketWriteString(conn *websocket.Conn, data string) error {
 	return websocket.Message.Send(conn, data)
 }
