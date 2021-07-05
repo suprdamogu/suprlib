be798ab059855f328cc1ceb76b68490f4b2800c1
suprdamogu <suprdamogu@hotmail.com>
2021-07-05 16:51:18 +0800

    panic error

--- errs.go
+++ errs.go
@@ -6,3 +6,9 @@ var MAP_KEY_NOT_FOUND_ERROR = errors.New("MAP_KEY_NOT_FOUND_ERROR")
 var JSON_MAP_TRANS_ERROR = errors.New("JSON_MAP_TRANS_ERROR")
 var NOT_FOUND_ERROR = errors.New("NOT_FOUND_ERROR")
 var DEAD_CODE = errors.New("DEAD_CODE")
+
+func PanicError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
